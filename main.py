3a6fbaba95985afa6cb16124e5f6faada3d99f46
matthewgieger <mattbju2013@gmail.com>
2016-11-16 23:52:49 -0500

    Removed debugging print statement

--- main.py
+++ main.py
@@ -14,43 +14,6 @@ def main():
     speaker = str(raw_input("Enter speaker of sermon: "))
     date = str(raw_input("Enter date of sermon: "))
 
-    print """ffmpeg -y -loop 1 -i "{image}"\
-              -i "{audio_file_name}"\
-              -t {audio_length} -pix_fmt yuv420p \
-              -vf "drawtext=fontcolor={text_color}:\
-                   fontfile={font_file_path}:\
-                   text='{title}\n\n{speaker}\n\n{date}':\
-                   fontsize={font_size}:\
-                   alpha={text_alpha}:\
-                   x={horizontal_position}:y={vertical_position}:\
-                   box={box}:\
-                   boxborderw={box_boarder_width}:\
-                   boxcolor={box_color},\
-                   scale={scale_width}:-2"\
-              "{output_location}/{output_file_name}{video_output_type}"\
-              """.format(audio_file_name=audio_file_name,
-                         box=settings.box,
-                         box_boarder_width=settings.box_boarder_width,
-                         box_color=settings.box_color,
-                         date=date,
-                         font_file_path=settings.font_file_path,
-                         font_size=settings.font_size,
-                         horizontal_position=settings.horizontal_position,
-                         image=image,
-                         audio_length=audio_length,
-                         output_file_name=output_file_name,
-                         output_location=settings.output_location,
-                         scale_width=settings.scale_width,
-                         speaker=speaker,
-                         text_alpha=settings.text_alpha,
-                         text_color=settings.text_color,
-                         title=title,
-                         vertical_position=settings.vertical_position,
-                         video_output_type=settings.video_output_type
-                         )
-
-
-
     os.system("""ffmpeg -y -loop 1 -i "{image}"\
               -i "{audio_file_name}"\
               -t {audio_length} -pix_fmt yuv420p \
