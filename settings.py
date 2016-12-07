4c6c01e8db5db2b1445741b4c48a426ee1a3ad4b
matthewgieger <mattbju2013@gmail.com>
2016-12-06 21:55:15 -0500

    updated .gitignore & changed font size to 75

--- settings.py
+++ settings.py
@@ -1,14 +1,14 @@
 border_color = "black"         # Color of border around text
-border_width = "2"              # Width of text border in pixels
+border_width = "2"             # Width of text border in pixels
 box = "0"                      # Yes=1 No=0 for box around text
-box_border_width = "10"       # Number of Pixel padding around text
+box_border_width = "10"        # Number of Pixel padding around text
 box_color = "black"            # Color of box surrounding text
 font_file_path = "/Library/Fonts/Arial.ttf"   # Path to font file to use for text
-font_size = "100"               # Font Size
+font_size = "75"               # Font Size
 horizontal_position = "(main_w/2) - (text_w/2)"  # Distance in Pixels from left
 output_location = "/Users/Matt/Desktop"  # Locaiton where output file is generated
 scale_width = "1280"           # Width of video in pixels
-text_alpha = "1"             # Opacity 0.0 - 1.0
+text_alpha = "1"               # Opacity 0.0 - 1.0
 text_color = "white"           # Color of text
 vertical_position = "(main_h/2) - (text_h/2)"       # Distance in Pixels from Top
 video_output_type = ".mp4"     # Extension of output file (only mp4 supported)
